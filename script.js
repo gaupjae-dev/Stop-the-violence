bb4dc6b95f22d1952845f3a9b90e21886382f605
gaupjae-dev <gaupjae@gmail.com>
2025-10-21 12:32:08 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function loadMissions() {
+    alert("MISSIONS LOG: Ready to dominate the court!");
+    // You would replace the 'alert' with code to load the actual missions menu later!
+}
+
+function loadMyPlayer() {
+    alert("MY PLAYER: Customizing your superstar...");
+}
+
+function loadOptions() {
+    alert("OPTIONS: Adjusting settings...");
+}
+
+function quitGame() {
+    alert("QUITTING: Thanks for playing!");
+}
